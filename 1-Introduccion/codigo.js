c77cea77a826d47ce2a6e209062c26658ee69486
Fabian Montes <67841089+Fabian2112@users.noreply.github.com>
2024-06-18 22:09:06 -0300

    Add files via upload

---
+++ 1-Introduccion/codigo.js
@@ -0,0 +1,106 @@
+
+recipiente = "papel";
+string = "cadena de texto";
+number = 23;
+booleano = true;
+
+
+let numero = 30;
+
+numero = 21;
+
+/*
+alert(numero);
+
+
+let numero1 = 45, numero2 = 65, numero3 = 28;
+
+alert(numero1);
+alert(numero2);
+alert(numero3);
+
+
+let numero4 = null;
+
+alert(numero4*recipiente);
+
+
+let negocio = prompt("DIME TU NEGOCIO");
+
+alert("bienvenido " + negocio);
+*/
+
+numero += 7
+
+document.write(numero)
+
+
+numero10 = 51
+numero10--
+
+document.write(numero10)
+
+
+saludo = "¡Hola Pedro!"
+pregunta = " ¿Como estas?"
+
+frase = saludo + pregunta
+
+document.write(frase)
+
+number1 = "12";
+number2 = 5;
+
+result = "" + number1 + number2
+
+document.write(result);
+
+result2 = number1.concat(number2) /*solo para las cadenas*/
+
+document.write(result2)
+
+
+name = "fabian montes"
+frase1 = `soy ${name} y estoy viciando `
+
+document.write(frase1)
+
+document.write(number1==number2)
+
+
+num1 = 5;
+num2 = 23;
+num3 = 24;
+num4 = 94;
+num5 = 90;
+
+op = (num1 < num2 || num2 < num3) && (!(num1 != num2) && num5 != num3)
+op = (true || true) && (false && true)
+op = true && false
+
+document.write(op)
+
+
+let alumno = prompt("tu nombre")
+
+if(alumno == "Fabian") {
+	alert("cabezon")
+}
+
+else if (alumno == "Roque") {
+	alert("Malardo")
+}
+
+else {
+	alert("nombre equivocado")
+}
+
+
+const nombre = "EMPREMARK";
+
+nombre	= "eloy";
+/*termina dando error (se puede ver en console)*/
+
+alert(nombre);
+
+
---
+++ 2-Arreglos/codigo.js
@@ -0,0 +1,26 @@
+
+alert("hola")
+
+
+let frutas = ["banana","manzana","pera", 5,9,"pedro"]
+
+document.write(frutas[5])
+
+
+//traeme el elemento que está en la posicion
+
+
+let pc1 = {
+	nombre : "EmpreMark",
+	procesador:"IntelCore I7"
+	ram:"16GB"
+	espacio:"1TB"
+}
+
+
+
+let pc2 = ["EmpreMark","IntelCore I7","16GB", "1TB"]
+
+
+
+
